a842e6f04ff51a2ac7d263d1d3d47a7c0f4cbf28
Davis Marques <dmarques@freshbits.io>
2019-11-15 15:12:44 +1100

    Home view sketch updates

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,71 +1,80 @@
 import { Github } from 'styled-icons/boxicons-logos/Github';
 import { Gitlab } from 'styled-icons/boxicons-logos/Gitlab';
 import React from 'react';
-import { Bacteria, Owl5 } from '../../components/icons';
+import { Bacteria, Owl5 } from '../../components/branding';
+import { FlexColumn, FlexRow } from '../../components/layout';
 import { Home } from './styles';
 
 class HomeContainer extends React.Component {
   render() {
     return (
       <Home>
-        <section className={'margin-top-l'}>
-          <h2>postdoc</h2>
-          <p className={'font-size-xs max-width-480'}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat.
-          </p>
-        </section>
+        <FlexRow alignItems={'flex-start'} justifyContent={'flex-start'}>
+          <FlexColumn style={{maxWidth: 480, width: 480}}>
+            <section className={'margin-top-l'}>
+              <h2>postdoc</h2>
+              <p className={'font-size-xs max-width-480'}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat.
+              </p>
+            </section>
+            <section>
+              <h4>Project Repositories</h4>
 
-        <section>
-          <h4>Project Repositories</h4>
-          <img src={Bacteria} />
+              <div className={'project'}>
+                <h6>Project Name</h6>
+                <Github style={{width: 24}}/>
+                <div className={'stats'}>
+                  pull requests
+                  issues
+                  contributors
+                  GitHub
+                </div>
+              </div>
+              <div className={'project'}>
+                <h6>Project Name</h6>
+                <Github style={{width: 24}}/>
+                <div className={'stats'}>
+                  pull requests
+                  issues
+                  contributors
+                  GitHub
+                </div>
+              </div>
+              <div className={'project'}>
+                <h6>Project Name</h6>
+                <div className={'stats'}>
+                  pull requests
+                  issues
+                  contributors
+                  GitHub
+                </div>
+              </div>
+              <div className={'project'}>
+                <h6>Project Name</h6>
+                <div className={'stats'}>
+                  pull requests
+                  issues
+                  contributors
+                  GitHub
+                </div>
+              </div>
+            </section>
+          </FlexColumn>
+          <FlexColumn style={{maxWidth: 480, width: 480}}>
+            <img src={Bacteria} style={{maxWidth: 480}}/>
+            <section className={'credits'}>
+              Credits
 
-          <div className={'project'}>
-            <h6>Project Name</h6>
-            <Github style={{width: 24}}/>
-            <div className={'stats'}>
-              pull requests
-              issues
-              contributors
-              GitHub
-            </div>
-          </div>
-          <div className={'project'}>
-            <h6>Project Name</h6>
-            <Github style={{width: 24}}/>
-            <div className={'stats'}>
-              pull requests
-              issues
-              contributors
-              GitHub
-            </div>
-          </div>
-          <div className={'project'}>
-            <h6>Project Name</h6>
-            <div className={'stats'}>
-              pull requests
-              issues
-              contributors
-              GitHub
-            </div>
-          </div>
-          <div className={'project'}>
-            <h6>Project Name</h6>
-            <div className={'stats'}>
-              pull requests
-              issues
-              contributors
-              GitHub
-            </div>
-          </div>
-        </section>
+              <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/turkkub" title="turkkub">turkkub</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+            </section>
+          </FlexColumn>
 
-        <section className={'credits'}>
-          Credits
+        </FlexRow>
 
-          <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/turkkub" title="turkkub">turkkub</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
-        </section>
+
+
       </Home>
     );
   }
