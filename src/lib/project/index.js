f26d704806da01b2d8534e1a7c083a836bdb13d3
Davis Marques <dmarques@freshbits.io>
2019-12-15 23:49:46 +1100

    Fix linting errors, messages

--- src/lib/project/index.js
+++ src/lib/project/index.js
@@ -1,15 +1,15 @@
-import Database from "../database";
-import path from "path";
-import Utils from "../utils";
+import Database from '../database';
+import path from 'path';
+import Utils from '../utils';
 
-const PROJECT_SETTINGS_FILE = "project.json";
+const PROJECT_SETTINGS_FILE = 'project.json';
 
 function getSettings(fp) {
   const p = path.join(fp, PROJECT_SETTINGS_FILE);
   return Utils.files.readJSON(p);
 }
 
-function updateIndex(fp) {
+export function updateIndex(fp) {
   return Database.loadFromDir(fp)
     .updateIndex()
     .write();
